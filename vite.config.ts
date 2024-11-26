01bc240803304b83b2ce9df74f051ede8b37da7b
fdg312 <ffedorovzx@gmail.com>
2024-11-27 00:45:14 +0500

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/baski-test-project',
+	base: '/baski-test-project/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
