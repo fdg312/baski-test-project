ac7db110016bd72106dcfbc6272dcecbd7b53605
fdg312 <ffedorovzx@gmail.com>
2024-11-27 01:01:43 +0500

    Prepair for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
-		<BrowserRouter>
+		<BrowserRouter basename='/baski-test-project'>
 			<App />
 		</BrowserRouter>
 	</QueryClientProvider>
