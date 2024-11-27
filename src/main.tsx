0245278ae1ff837a4fbf7a57463ddce9b622d88e
fdg312 <ffedorovzx@gmail.com>
2024-11-27 15:17:45 +0500

    Add a browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
@@ -8,8 +8,8 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
-		<HashRouter basename='/baski-test-project'>
+		<BrowserRouter basename='/baski-test-project'>
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	</QueryClientProvider>
 )
